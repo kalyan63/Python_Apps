1dd599394f336d420b6bbeb76e76ecb6069c83b4
kalyan63 <skalyanreddy123@gmail.com>
2022-02-11 19:37:31 +0530

    Commit3

--- samp_mob.py
+++ samp_mob.py
@@ -9,7 +9,6 @@ class HelloKivy(App):
     # This returns the content we want in the window
     def build(self):
   
-        # Return a label widget with Hello Kivy
         return Label(text ="Hello Geeks for new start")
   
 helloKivy = HelloKivy()
